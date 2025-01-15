0e3c49853e7fc7ac754e11e3c291d7f735a94cf5
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-01-15 10:06:59 +0545

    string_reverse_stack

--- stack.py
+++ stack.py
@@ -9,15 +9,63 @@
 
 # this is also a way to implement stack in python but is not a recommended one
 
+# from collections import deque
+
+# stack = deque()
+# stack.append("Nirajan")
+# stack.append("hello")
+# stack.append(123)
+# stack.append("nirajan123")
+
+# print(stack)
+# print(stack.pop())
+# print(stack.pop())
+# print(stack)
+
+# let's make a stack class of our own using the deque collection
+
 from collections import deque
 
-stack = deque()
-stack.append("Nirajan")
-stack.append("hello")
-stack.append(123)
-stack.append("nirajan123")
+class Stack:
+    def __init__(self):
+        self.container = deque()
 
-print(stack)
-print(stack.pop())
-print(stack.pop())
-print(stack)
+    def push(self, val):
+        return self.container.append(val)
+
+    def pop(self):
+        return self.container.pop()
+    
+    def is_empty(self):
+        if len(self.container) == 0:
+            return True
+        else:
+            return False
+        
+    def peek(self):
+        return self.container[-1]
+    
+    def len(self):
+        return len(self.container)
+    
+    def index(self):
+        return self.container.index
+    
+    def __iter__(self):
+        return iter(self.container)
+
+
+def reverse_string(string):
+    stack1 = Stack()
+    for item in string:
+        stack1.push(item)
+
+    for _ in range(stack1.len()):
+        print(stack1.peek())
+
+
+if __name__ == "__main__":
+    string = input("Enter the string you want to reverse.")
+    reverse_string(string)
+
+
