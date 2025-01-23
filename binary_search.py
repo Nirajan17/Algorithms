9ab6e4778149447d1578fdde6f8a9e15c4d295fa
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-01-23 07:24:19 +0545

    bs-using-recursion

--- binary_search.py
+++ binary_search.py
@@ -14,6 +14,23 @@ def binary_search(values, value):
 
     return -1
 
+def bs_recursion(values, value, left_index, right_index):
+    if right_index < left_index:
+        return -1
+    
+    mid_index = (left_index + right_index) // 2
+    middle_number = values[mid_index]
+
+    if values[mid_index] == value:
+            return mid_index
+
+    if middle_number < value:
+        left_index = mid_index + 1
+    else :
+        right_index = mid_index - 1
+
+    return bs_recursion(values, value, left_index, right_index)
+
 
 def linear_search(values, value):
     for i,v in enumerate(values):
@@ -28,3 +45,7 @@ if __name__=="__main__":
     print(binary_search(values, 12))
     print(binary_search(values, 129))
     print(binary_search(values, 100))
+    print("\n")
+    print(bs_recursion(values, 12, 0, len(values)))
+    print(bs_recursion(values, 129, 0, len(values)))
+    print(bs_recursion(values, 100, 0, len(values)))
