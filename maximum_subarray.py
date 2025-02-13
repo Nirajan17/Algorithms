0d4d6d070b5fea7d1fddd7a530918e03c13fba71
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-13 08:24:05 +0545

    ineffiecient solution

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,2 +1,14 @@
 # given an array find the subarray with the largest sum.
 
+class Solution:
+    def maxSubarray(self, nums):
+        max_array = []
+        for i in range(len(nums)):
+            sum = 0
+            sum_array = []
+            for j in range(i,len(nums)):
+                sum += nums[j]
+                sum_array.append(sum)
+            max_array.append(max(sum_array))
+        max_number = max(max_array)
+        return max_number
