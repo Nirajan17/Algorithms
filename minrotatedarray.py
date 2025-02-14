dcd93993450b0c46802e45c7bafef5e15faf9903
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-14 20:33:15 +0545

    searching in rotated sorted array

--- minrotatedarray.py
+++ minrotatedarray.py
@@ -3,4 +3,13 @@
 # time complexity should be O(logn)
 
 class Solution:
-    
+    def minRotated(self, nums):
+        left, right = 0, len(nums)-1
+
+        while(left < right):
+            mid = (left+right) // 2
+            if nums[mid] > nums[left]:
+                right = mid-1
+            else:
+                left = mid
+        return nums[right]
