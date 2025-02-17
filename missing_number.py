e61d144528cde66bdcb20993129d662eaacfc925
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-17 16:42:47 +0545

    O(n^2)

--- missing_number.py
+++ missing_number.py
@@ -1,2 +1,19 @@
 # finding a number missing in the array 
 
+class Solution:
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        nums_length = len(nums)
+        for i in range(nums_length+1):
+            if i not in nums:
+                return i
+            
+
+# solution above is a bruteforce approach making it very inefficient
+# it's time complexity is O(n^2)
+
+# let's try optimal solution
