93d84ada3f797d265faae2495a28cc8a8a856b5a
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-17 12:36:18 +0545

    counting bits

--- number_of_1bits.py
+++ number_of_1bits.py
@@ -11,3 +11,17 @@ class Solution:
 
             n_bin = n_bin/10
         return count
+    
+# the above code runs for 32 times if the a single number is represented using 32 bits
+# but it can be done in way lesser
+
+# basically do n&(n-1) until it reaches 0
+
+class Solution:
+    def hammingWeight(self, n):
+        n = int(bin(n)[2:])
+        count = 0
+        while n&(n-1):
+            count += 1
+            n= n&(n-1)
+        return count
