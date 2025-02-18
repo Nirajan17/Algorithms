22f742995a0216588ea3523cd6c7d52b9b90bd28
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-18 21:46:12 +0545

    done but not understood

--- coin_change.py
+++ coin_change.py
@@ -1,18 +1,17 @@
 # you are given an integer array of coins having different denominations 
 # using those coins, you have to make up to "amount" using fewest number of coins
 
+# Input: coins = [1,2,5], amount = 11
+
 class Solution:
     def coinChange(self, coins, amount):
-        result = 0
-        if amount in coins:
-            return 1
-        
-        coins.sort()
+        dp = [amount + 1] * (amount+1)
+        dp[0] = 0
 
-        for i in range(len(coins)):
-            result = amount // coins[len(coins)-1-i]
-            num = result * coins[len(coins)-1-i]
-            amount = amount - num
-            if amount in coins:
-                return result
+        for i in range(1, amount+1):
+            for c in coins:
+                if i-c>=0:
+                    dp[i] = min(1+dp[i-c], dp[i])
+        
+        return dp[amount] if dp[amount] != amount+1 else -1
 
