28f578fc2ebb2d2693462a0d7a1619485251043b
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-19 22:24:54 +0545

    done

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -2,17 +2,18 @@
 
 class Solution:
     def longestSubsequence(self, nums):
-        lss = [1]*(len(nums))
-        # lss[len(nums)-1] = 1
+        if not nums:
+            return 0
 
-        for i in range(len(nums)-2, -1, -1):
-            j=i+1
-            while j<len(nums) and nums[j]>nums[i]:
-                if nums[j] > nums[i]:  # check if the array have greater values than of i'th
-                    lss[i] = max(lss[i], 1 + max(lss[i+1:])) # not 1 + lss[i+1] but 1+max(lss>i)
-                j += 1
+        lss = [1] * len(nums)  
         
-        return max(lss)
+        for i in range(len(nums) - 2, -1, -1):  
+            for j in range(i + 1, len(nums)): 
+                if nums[j] > nums[i]:  
+                    lss[i] = max(lss[i], 1 + lss[j]) 
+        
+        return max(lss)  
+
     
 if __name__=="__main__":
     nums = [4,10,4,3,8,9]
