da0ed38bce7eb792e7d38d488449edabb9f2095b
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-19 22:21:53 +0545

    all cases not passed

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -3,16 +3,19 @@
 class Solution:
     def longestSubsequence(self, nums):
         lss = [1]*(len(nums))
-        lss[len(nums)-1] = 1 # last index has subquence of length 0
+        # lss[len(nums)-1] = 1
 
         for i in range(len(nums)-2, -1, -1):
-            if nums[i+1] > nums[i]:
-                lss[i] = max(lss[i], 1 + lss[i+1])
+            j=i+1
+            while j<len(nums) and nums[j]>nums[i]:
+                if nums[j] > nums[i]:  # check if the array have greater values than of i'th
+                    lss[i] = max(lss[i], 1 + max(lss[i+1:])) # not 1 + lss[i+1] but 1+max(lss>i)
+                j += 1
         
         return max(lss)
     
 if __name__=="__main__":
-    nums = [1,2,4,3,5]
+    nums = [4,10,4,3,8,9]
     solution = Solution()
     value = solution.longestSubsequence(nums)
     print(f"max subsequeunce is {value}")
