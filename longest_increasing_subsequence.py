7c61a412ca935fba6a09bf545e9b2e837eec1f12
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-19 21:37:22 +0545

    solving

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -1,2 +1,14 @@
 # finding the longest increasing subsequence in an array
 
+class Solution:
+    def longestSubsequence(self, nums):
+        lss = [1]*(len(nums))
+        lss[len(nums)-1] = 0 # last index has subquence of length 0
+
+        for i in range(len(nums)-2, 1, -1):
+            if nums[i+1] > nums[i]:
+                lss[i] = max(lss[i], 1 + lss[i+1])
+        
+        return max(lss)
+    
+
