650e363532f15460e00f64a8300cee4286a34203
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-19 21:52:41 +0545

    debugged but no issue found mental

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -3,12 +3,16 @@
 class Solution:
     def longestSubsequence(self, nums):
         lss = [1]*(len(nums))
-        lss[len(nums)-1] = 0 # last index has subquence of length 0
+        lss[len(nums)-1] = 1 # last index has subquence of length 0
 
-        for i in range(len(nums)-2, 1, -1):
+        for i in range(len(nums)-2, -1, -1):
             if nums[i+1] > nums[i]:
                 lss[i] = max(lss[i], 1 + lss[i+1])
         
         return max(lss)
     
-
+if __name__=="__main__":
+    nums = [1,2,4,3,5]
+    solution = Solution()
+    value = solution.longestSubsequence(nums)
+    print(f"max subsequeunce is {value}")
