fb21957c7b9573442e3a7e6e12833f7a231cae7e
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-21 12:21:12 +0545

    size of matrix matters

--- longest_common_subsequence.py
+++ longest_common_subsequence.py
@@ -1,2 +1,19 @@
 # finding the length of the longest common subsequence from two strings
 
+class Solution:
+    def longcomSubsequence(self, text1, text2):
+        dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
+        
+        for i in range(len(text1)-1, -1, -1):
+            for j in range(len(text2)-1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        return dp[0][0]
+    
+
+if __name__=="__main__":
+    sol = Solution()
+    r_value = sol.longcomSubsequence("he", "hel")
+    print(f"Longest subsequence is {r_value}")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,2 @@
-    pi = partition(elements, start, end)
-        quick_sort(elements, start, pi-1)
-        quick
+
+    
