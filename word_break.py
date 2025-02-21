fbe9c32b7b25a6250a107ffaa402d0435635d0b2
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-21 13:57:10 +0545

    recursion gives worst time complexity, it works but not good enoguh

--- word_break.py
+++ word_break.py
@@ -1,2 +1,18 @@
 # given a string s and a word dictionary wordDict, we have to find if s can be segmented into words that are in wordDict
 
+# string = "leetcode" and wordDict = ["leet", "code", "fun"]
+
+class Solution:
+    def wordBreak(self, s, word_dict):
+
+        if not s:
+            return True  
+        
+        for i in range(1, len(s) + 1): 
+            first = s[:i]
+            
+            if first in word_dict:
+                if self.wordBreak(s[i:], word_dict):  # Check the remaining part
+                    return True
+        
+        return False
