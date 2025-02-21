8760840bb4fb467945ed71b8463287358ad95a9f
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-21 19:40:29 +0545

    noted

--- word_break.py
+++ word_break.py
@@ -10,13 +10,14 @@ class Solution:
         for i in range(len(s)-1, -1, -1):
             for w in word_dict:
                 if (i+len(w) <= len(s)) and s[i:i+len(w)] == w:
-                    dp[i] = dp[i+len(w)]
+                    dp[i] = dp[i+len(w)]  # why this, check for the example "catsandog", you will realize why, it might come to your mind that we could have directly written "True" there but NO!!
                 if dp[i]:
                     break
         return dp[0]
     
 if __name__=="__main__":
-    s = "helo"
-    word_dict = ["hell", "hel", "o", "he", "h"]
+    s = "catsandog"
+    word_dict = ["cats","dog","sand","and","cat"]
+
     wb = Solution()
     print(wb.wordBreak(s,word_dict))
