822508971d410aeb4130c10801fabff19c4a68eb
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-24 15:41:57 +0545

    wrong approach

---
+++ house_robber.py
@@ -0,0 +1,12 @@
+# given an integer array, find the maximum amount you can get from that array(not including the adjacent elements)
+
+class Solution:
+    def houseRobber(self, nums):
+        
+    
+
+if __name__=="__main__":
+    nums = [2,7,9,3,1]
+    s = Solution()
+    value = s.houseRobber(nums)
+    print(value)
