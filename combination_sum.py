6bdc6e3e7bc80c5cea52e77f0318a8bb66c1c6af
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-24 13:22:17 +0545

    k vayo vayo!!

--- combination_sum.py
+++ combination_sum.py
@@ -4,6 +4,32 @@
 
 class Solution:
     def combinationSum(self,candidates, target):
+        result = []
+        cur = []
 
-        def recurSum(i, ):
+        def recurSum(i):
+            cur.append(candidates[i])
+            total = sum(cur)
+
+            if total == target:
+                result.append(cur)
+                return
+            
+            if i >= len(candidates) or total > target:
+                return
+            
+            recurSum(i)
+            cur.clear()
+            recurSum(i+1)
+
+        recurSum(0)
+        return result
+    
+
+if __name__=="__main__":
+    candidates = [2,3,6,7]
+    target = 7
+    s = Solution()
+    value = s.combinationSum(candidates, target)
+    print(value)
             
