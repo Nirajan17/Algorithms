d0bba6ac9061727cbe6ebfd944b3bef61a236308
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-24 14:42:26 +0545

    needed lots of debugging to understand

--- combination_sum.py
+++ combination_sum.py
@@ -7,7 +7,6 @@ class Solution:
         result = []
 
         def recurSum(i, cur, total):
-
             if total == target:
                 # we need to specifically append the copy of the list here because if we append cur directly, reference to cur will be appended which means if cur is changed later in recursive calls the appended cur value will also get changed, ie mutated.
                 result.append(cur.copy())
@@ -24,7 +23,6 @@ class Solution:
         recurSum(0,[],0)
         return result
     
-
 if __name__=="__main__":
     candidates = [2,3,6,7]
     target = 7
