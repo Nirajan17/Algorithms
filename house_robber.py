7c46df0e625da12b46d6854c7b714a15164ae6d3
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-24 20:51:40 +0545

    beats 100% users in leetcode in time complexity

--- house_robber.py
+++ house_robber.py
@@ -2,11 +2,17 @@
 
 class Solution:
     def houseRobber(self, nums):
+        dp = [1] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[:2])
         
-    
+        for i in range(2,len(nums)):
+            dp[i] = max(nums[i]+dp[i-2], max(dp[i-1], nums[i]))
+        
+        return dp[len(nums)-1]
 
 if __name__=="__main__":
-    nums = [2,7,9,3,1]
+    nums = [2,1,1,2]
     s = Solution()
     value = s.houseRobber(nums)
     print(value)
