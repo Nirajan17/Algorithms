cf7ed0f6b8d31270002d12eca88c4ac817b0210a
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-25 08:09:58 +0545

    needed inspection

--- house_robberII.py
+++ house_robberII.py
@@ -3,12 +3,17 @@
 
 class Solution:
     def houseRobber(self, nums):
+        if not nums:
+                return 0
+        if len(nums) == 1:
+            return nums[0]
         
         def helper(houses):
-            if not nums:
+            if not houses:
                 return 0
-            if len(nums) == 1:
-                return nums[0]
+            if len(houses) == 1:
+                return houses[0]
+            
             dp = [1] * len(houses)
             dp[0] = houses[0]
             dp[1] = max(houses[:2])
@@ -18,10 +23,10 @@ class Solution:
             
             return dp[len(houses)-1]
         
-        return max(helper(nums[:len(nums)-1]), helper(nums[1:]))
+        return max(helper(nums[:len(nums)]), helper(nums[1:]))
     
 if __name__=="__main__":
-    nums = [2,3,1]
+    nums = [1]
     s = Solution()
     result = s.houseRobber(nums)
     print(result)
