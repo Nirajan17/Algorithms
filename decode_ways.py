9a5739c1be806ff499c7eb3c61c2d6fd9bbe86f8
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-25 21:34:12 +0545

    runtime exceeded

--- decode_ways.py
+++ decode_ways.py
@@ -2,4 +2,25 @@
 
 class Solution:
     def decodeWays(self, s):
-        pass
+        count = 0
+        
+        def dfs(i):
+            nonlocal count
+            if i == len(s):
+                count += 1
+                return
+            
+            if s[i] == "0":
+                return 
+            
+            dfs(i+1)
+            if i+1 < len(s) and (s[i]=="1" or (s[i] == "2" and s[i+1] in "0123456")):
+                dfs(i+2)
+
+        dfs(0)
+        return count
+
+if __name__=="__main__":
+    s = "06"
+    s1 = Solution()
+    print(s1.decodeWays(s))
