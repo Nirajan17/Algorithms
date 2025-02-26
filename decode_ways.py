b844a012929e1728b37e866b62f2e2ea245ca753
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-26 09:10:50 +0545

    using variable count for increment don't work well in recursive calls

--- decode_ways.py
+++ decode_ways.py
@@ -1,9 +1,11 @@
 # decoding the given string of numbers into alphabets in multiple ways
 
+from functools import lru_cache
+
 class Solution:
     def decodeWays(self, s):
         count = 0
-        
+        @lru_cache(None)
         def dfs(i):
             nonlocal count
             if i == len(s):
@@ -14,13 +16,13 @@ class Solution:
                 return 
             
             dfs(i+1)
-            if i+1 < len(s) and (s[i]=="1" or (s[i] == "2" and s[i+1] in "0123456")):
+            if i+1 < len(s) and ((s[i]=="1" and i<2) or ((s[i] == "2" and s[i+1] in "0123456") and i<2)):
                 dfs(i+2)
 
         dfs(0)
         return count
 
 if __name__=="__main__":
-    s = "06"
+    s = "1111"
     s1 = Solution()
     print(s1.decodeWays(s))
