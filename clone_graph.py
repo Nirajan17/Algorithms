7bd3d457223e54267d3f176e3fe92a111df8dc75
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-26 22:15:29 +0545

    why clone??

---
+++ clone_graph.py
@@ -0,0 +1,6 @@
+# cloning the graph and returning the new reference of a node 
+# example of why we are doing this problem
+
+# original_graph = {1:[2,3], 2:[3,4], 3:[1,4]}
+# copied_graph = original_graph ,if we do this,changing the copied_graph will also bring change in former
+
