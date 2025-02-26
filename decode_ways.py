49479cf4bbd8827b9b512edaf80a18f69d4cb36b
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-26 09:21:06 +0545

    now i understood

--- decode_ways.py
+++ decode_ways.py
@@ -1,28 +1,29 @@
 # decoding the given string of numbers into alphabets in multiple ways
 
 from functools import lru_cache
+from time_wrapper import time_it
 
 class Solution:
+    @time_it
     def decodeWays(self, s):
-        count = 0
-        @lru_cache(None)
+        n = len(s)
+        @lru_cache
         def dfs(i):
-            nonlocal count
-            if i == len(s):
-                count += 1
+            if i == n:
+                return 1
+            if s[i] == "0":
                 return
             
-            if s[i] == "0":
-                return 
-            
-            dfs(i+1)
-            if i+1 < len(s) and ((s[i]=="1" and i<2) or ((s[i] == "2" and s[i+1] in "0123456") and i<2)):
-                dfs(i+2)
-
-        dfs(0)
-        return count
+            ways = dfs(i+1)
+            if i + 1 < len(s) and (s[i] == "1" or (s[i] == "2" and s[i+1] in "0123456")):
+                ways += dfs(i+2)
+            return ways
+        return dfs(0)
 
 if __name__=="__main__":
-    s = "1111"
+    s = "1111111111111111"
     s1 = Solution()
     print(s1.decodeWays(s))
+
+
+# using lrucahche, runtime is 19 units but, without using lrucahce it took 81 units for "111111"
