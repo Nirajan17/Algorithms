0d022082ad0a258bdefbf3f37f2530e1ebcd50df
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-26 10:51:43 +0545

    math is dope

--- unique_paths.py
+++ unique_paths.py
@@ -1,19 +1,19 @@
 # finding number of unique paths in a grid from 0,0 to m,n
 
-# class Solution:
-#     def uniqueWays(self, m, n):
-#         grid = [[1 for _ in range(m)] for _ in range(n)]
+class Solution:
+    def uniqueWays(self, m, n):
+        grid = [[1 for _ in range(m)] for _ in range(n)]
 
-#         for i in range(m-2, -1, -1):
-#             for j in range(n-2, -1, -1):
-#                 if i < m and j < n:
-#                     grid[j][i] = grid[j][i+1] + grid[j+1][i]
+        for i in range(m-2, -1, -1):
+            for j in range(n-2, -1, -1):
+                if i < m and j < n:
+                    grid[j][i] = grid[j][i+1] + grid[j+1][i]
 
-#         return grid[0][0]
+        return grid[0][0]
     
-# if __name__=="__main__":
-#     s = Solution()
-#     print(s.uniqueWays(3,7))
+if __name__=="__main__":
+    s = Solution()
+    print(s.uniqueWays(3,7))
 
 # let's try the combinatorial solution
 
