ed82391a6bfffec629d2d64d2cc29cd50cefba8c
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-02-27 13:28:00 +0545

    finally some work done

--- clone_graph.py
+++ clone_graph.py
@@ -2,19 +2,50 @@
 # direct assignment of variables creates reference to that object so changing one will reflect in assigned varibale, so we need to make the copy 
 
 class Node(object):
-    def __init__(self, val=None, neighbours=None):
+    def __init__(self, val=None, neighbors=None):
         self.val = val
-        self.neighbours = neighbours
+        self.neighbors = neighbors if neighbors is not None else []
     
     def display(self):
         print(f"Node's value is {self.val}")
-        print(f"It's neighbours are {self.neighbours}")
+        print(f"It's neighbors are {[neighbor.val for neighbor in self.neighbors]}")
 
 
-adjList = [[2,4],[1,3],[2,4],[1,3]]
 
-n=[[] for i in range(len(adjList)-1)]
-for i in range(len(adjList)-1):
-    n[i] = Node(i, adjList[i])
+class Solution(object):
+    def cloneGraph(self, node):
+        hash_map = {}
 
-print(n[1].display())
+        def dfs(n):
+            if n in hash_map:
+                return hash_map[n]
+            
+            copy_n = Node(n.val)
+            hash_map[n] = copy_n
+
+            for ng in n.neighbors:
+                copy_n.neighbors.append(dfs(ng))
+
+            return copy_n
+        
+        return dfs(node) if node else None
+    
+
+
+def graphBuilder(adj_list):
+    nodes = [Node(i+1)  for i in range(len(adj_list))]
+
+    for i, neighbors in enumerate(adj_list):
+        nodes[i].neighbors = [nodes[nei-1] for nei in neighbors]
+
+    return nodes
+
+if __name__=="__main__":
+    adj_list = [[2,4],[1,3],[2,4],[1,3]]
+
+    nodes = graphBuilder(adj_list)
+
+    s = Solution()
+
+    cloned_node = s.cloneGraph(nodes[1])
+    cloned_node.display()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,2 @@
 
-# for u, v in edge_list:
-#     adj_matrix[u][v] = 1
-
-# print(adj_matrix)
+    #     node.display()
