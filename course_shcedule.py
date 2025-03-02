eef0eaf7bdcb42ce0f09912d58b038fad46ad628
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-03-02 12:02:27 +0545

    long time

--- course_shcedule.py
+++ course_shcedule.py
@@ -30,3 +30,37 @@ class Solution:
 if __name__=="__main__":
     s = Solution()
     print(s.courseSchedule(5,[[0,1],[0,2],[1,3],[1,4],[3,4]]))
+
+
+
+
+
+
+
+# let's try coding this solution again
+
+class Solution:
+    def courseSchedule(self, numcourses, prerequisities):
+        pre_map = { i:[] for i in range(numcourses)}
+
+        for v1, v2 in prerequisities:
+            pre_map[v1].append(v2)
+
+        visit_set = set()
+        def dfs(course):
+            if course in visit_set:
+                return False
+            if pre_map[course] == []:
+                return True
+            
+            visit_set.add(course)
+            for pre in prerequisities:
+                if not dfs(pre): return False
+
+            visit_set.remove(course)
+            pre_map[course] = []
+            return True
+        
+        for course in numcourses:
+            if not dfs(course): return False
+        return True
