158376200e55f7565fbac5af3639a2ae1dd29791
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-03-05 10:29:04 +0545

    beats 100%

--- check_if_n_power_of3.py
+++ check_if_n_power_of3.py
@@ -6,15 +6,19 @@
 def powerThree(n):
     i = 0
     value = n
+    used = set()
     while value > 0:
 
         while 3**i <= value:
             i += 1
         i -= 1
+        if i in used:
+            return False
+        used.add(i)
         sub = 3**i
         value = value - sub
         i = 0
 
     return value == 0
 
-print(powerThree(21))
+print(powerThree(91))
