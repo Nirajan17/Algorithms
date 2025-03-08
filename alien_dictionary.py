8580c924160af23ce56b3e2bed1d70b980acc750
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-03-08 11:02:54 +0545

    first phase

--- alien_dictionary.py
+++ alien_dictionary.py
@@ -1,2 +1,16 @@
 # finding the sequence(sorting) of letters in new alien language
 
+def aleinLanguage(words):
+    adj_list = { c:set() for w in words for c in w }  # here dict is needed, why check by making list
+    for i in range(len(words)-1):
+        min_len = min(len(words[i]), len(words[i+1]))
+        
+        for j in range(min_len):
+            if words[i][j] != words[i+1][j]:
+                adj_list[words[i][j]].add(words[i+1][j])
+
+    return adj_list
+
+
+words = ["wrt","wrf","er","ett","rftt"]
+print(aleinLanguage(words))
