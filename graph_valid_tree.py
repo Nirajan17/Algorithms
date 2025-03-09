c127038cf2f0fcf7d1fa0621e51dcfdf427cccf6
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-03-09 11:29:30 +0545

    errrrrrrr

---
+++ graph_valid_tree.py
@@ -0,0 +1,38 @@
+# checking if the given graph is a valid tree
+
+# a graph is a tree iff :: 
+    # all of it's nodes are connected
+    # graph doesn't have a cycle
+
+# given :: n nodes and list of undirected edges
+
+# Input: n = 5 edges = [[0, 1], [0, 2], [0, 3], [1, 4]]
+
+def graphValidTree(n: int, edges: list[list[int]]) -> bool:
+    # let's first make the adjacency list of graph
+    adj_list = { i: set() for i in range(n) }
+    for n1, n2 in edges:
+        adj_list[n1].add(n2)
+        adj_list[n2].add(n1)  # to represent that it is a undirected graph
+
+    # let's check for a valid tree
+    visit = set()
+    prev = -1
+    def dfs(node):
+        nonlocal prev
+        if node in visit:
+            return False
+        visit.add(node)
+        for nei in adj_list[node]:
+            if nei != prev:
+                prev = node
+                if not dfs(nei):
+                    return True
+
+    return True if dfs(0) and (n == len(visit)) else False
+
+edges = [[0, 1], [0, 2], [0, 3], [1, 4]]
+# edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]]
+# edges = [[0,1], [0,2], [1,2]]
+
+print(graphValidTree(5, edges))
