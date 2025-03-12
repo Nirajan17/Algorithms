bc33b5374fb1750652cb0ab557739e9c83b6e2e4
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-03-12 10:39:47 +0545

    edited

--- 6.py
+++
@@ -1,8 +0,0 @@
-# knowing the index of a particular value in an array
-
-numbers = [2,3,4,5,4]
-
-for i in range(len(numbers)):
-    if numbers[i] == 4:
-        print(f"the number is in {i}th place.")
-        
--- 7.py
+++
@@ -1,6 +0,0 @@
-numbers = [2,3,4,5,4]
-
-numbers.insert(3,23)
-numbers.remove(23)
-numbers.pop(1)
-print(numbers)
