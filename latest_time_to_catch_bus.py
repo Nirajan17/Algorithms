fc1e4a346f8f95b6c36dbc4ae25e6f4f557f9435
nirajan_paudel <nirajanpaudel33@gmail.com>
2025-03-12 09:42:14 +0545

    done finally

--- latest_time_to_catch_bus.py
+++ latest_time_to_catch_bus.py
@@ -5,26 +5,39 @@
 def latestTimeCatchBus(buses, passengers, capacity):
     buses.sort()
     passengers.sort()
-    passengers_copy = passengers.copy()
 
-    for bus in buses:
-        for passenger in passengers:
-            if passenger < bus and passenger in passengers_copy:
-                for _ in range(capacity):
-                    current_passenger = passengers_copy.remove(passenger)
-            
-    # here we have current passenger in current_passenger variable, now we need to check both ways whether there is a time that we can reach to catch the bus
+    passenger_idx = 0
+    bus_idx = 0
 
-    if buses[-1] > current_passenger:
-        while current_passenger+1 not in passengers:
-            current_passenger += 1
-        return current_passenger
+    # simulation of passenger boarding bus
+    while bus_idx < len(buses) and passenger_idx < len(passengers):
+        current_capacity = 0
+        while passengers[passenger_idx]<=buses[bus_idx] and current_capacity<capacity and passenger_idx<len(passengers):
+            passenger_idx += 1
+            current_capacity += 1
+        bus_idx += 1
+
+    # now let's calculate the latest time
+    if passenger_idx<len(passengers):   # not all boarded
+        if current_capacity == capacity and passenger_idx>=0:  # there is no space in last bus
+            last_boarded = passengers[passenger_idx]
+            time = last_boarded-1
+            while time in passengers and time >= 0:
+                time -= 1
+            return time if time >= 0 else -1   # if valid time is not found, return -1
+    
+        else:   # there is space in bus so arrive unit time before the bus leaves
+            time = buses[bus_idx-1]
+            while time in passengers and time>=0:
+                time -= 1
+            return time if time>=0 else -1 
+    # all boarded
     else:
-        while current_passenger-1 not in passengers:
-            current_passenger -= 1
-        return current_passenger
+        if passenger_idx <= (len(buses) * capacity):
+            time = buses[-1]
+            while time in passengers and time >= 0:
+                time -= 1
+            return time if time >= 0 else -1
+    return buses[-1]
 
-buses = [10,20]
-passengers = [2,17,18,19]
-capacity = 2
-print(latestTimeCatchBus(buses, passengers, capacity))
+print(latestTimeCatchBus([10, 20], [2, 17, 18, 19], 2))
